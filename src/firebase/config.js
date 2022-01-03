fb09b38d72586848410c5170ecda2d7edf2e88f3
BraianMendes <00.braian.dev@gmail.com>
2022-01-02 22:06:44 -0300

    Update with Env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import '@firebase/firestore';
 import Config from 'react-native-config'
 
 const firebaseConfig = {
-  apiKey: Config.FIREBASE_API_KEY,
-  authDomain: Config.FIREBASE_AUTH_DOMAIN,
-  databaseURL: Config.FIREBASE_DATABASE_URL,
-  projectId: Config.FIREBASE_PROJECT_ID,
-  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
-  appId: Config.FIREBASE_APP_ID,
+  apiKey: `${Config.FIREBASE_API_KEY}`,
+  authDomain: `${Config.FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${Config.FIREBASE_DATABASE_URL}`,
+  projectId: `${Config.FIREBASE_PROJECT_ID}`,
+  storageBucket: `${Config.FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${Config.FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${Config.FIREBASE_APP_ID}`,
 };
 
 if (!firebase.apps.length) {
